519d906691958f2a2904d839d1d21a11927d8d14
Gabriel Lima <nickspoot@gmail.com>
2020-04-08 11:26:39 -0300

    creating components: home, feed, navbar and footer

--- blogPessoal/src/app/app.module.ts
+++ blogPessoal/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FeedComponent } from './feed/feed.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FeedComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,

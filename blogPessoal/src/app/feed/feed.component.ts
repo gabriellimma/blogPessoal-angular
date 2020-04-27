e4b1e9e4ffcf31d0bc57b7b2413484b72a43f85e
Gabriel Lima <nickspoot@gmail.com>
2020-04-26 21:29:06 -0300

    created getallpostagens module on feed component

--- blogPessoal/src/app/feed/feed.component.ts
+++ blogPessoal/src/app/feed/feed.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { PostagemService } from '../service/postagem.service';
+import { Postagem } from '../model/Postagem';
 
 @Component({
   selector: 'app-feed',
@@ -7,9 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FeedComponent implements OnInit {
 
-  constructor() { }
+  listaPostagens: Postagem[]
 
-  ngOnInit(): void {
+  constructor(private postagemService: PostagemService) { }
+
+  ngOnInit() {
+    this.findallPostagens()
+  }
+
+  findallPostagens() {
+    this.postagemService.getAllPostages().subscribe((resp: Postagem[]) => {
+      this.listaPostagens = resp
+    })
   }
 
 }
